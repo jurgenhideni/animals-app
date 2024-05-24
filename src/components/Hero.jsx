fe0ec54b6615056c703bdd1cedbaf794e147b7d0
Jurgen Hideni <jurgenhideni@gmail.com>
2024-05-24 14:14:23 +0200

    &apos errors fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
       <h1 className={`${luckiest.className} max-md:px-2 text-[2.7rem] max-w-[450px] text-center`}>Your Ultimate Guide to Pet Information</h1>
       <p className='max-w-[500px] max-md:max-w-[320px] text-slate-500 text-[0.9rem] text-center'>
       Discover a world of fascinating pets, from adorable 
-      cats and loyal dogs to exotic birds. Whether you're 
+      cats and loyal dogs to exotic birds. Whether youre 
       looking to adopt a new furry friend or simply want 
       to learn more about your favorite animals,
        our comprehensive guide has everything you need.
@@ -41,7 +41,7 @@ const Hero = () => {
 
 <div className='flex max-lg:flex-wrap justify-center gap-5 mx-auto w-fit'>
     {breedData.cats.map((cat)=>(
-  <div className='shadow-md w-fit bg-white text-center max-lg:px-1 max-lg:py-2 p-3 rounded-md hover:scale-105'>
+  <div key={cat.id} className='shadow-md w-fit bg-white text-center max-lg:px-1 max-lg:py-2 p-3 rounded-md hover:scale-105'>
    <Image src={cat.url} width={150} height={100}/>
    <span>{cat.name}</span>
   </div>
